60dbe8b46fb995ab62d8cc6d09cb040b23982f2c
Komal Kumari <70913086+komal-1@users.noreply.github.com>
2024-04-08 22:40:09 +0530

    Create TrafficLights.py
    Traffic Light Code

---
+++ TrafficLights.py
@@ -0,0 +1,9 @@
+light = input("light : ")
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("look")
+elif(light == "green"):
+    print("go")
+else:
+    print("This is not a traffic light colour.")
